e04ee840fa9201db1af48ecf509560fc48819b12
Hyunwoo <>
2022-09-08 22:24:51 +0900

    영웅별 자주가는 레인 추가

--- DbMaker/main.py
+++ DbMaker/main.py
@@ -1,3 +1,4 @@
+import ipaddress
 import sqlite3
 import requests
 import json
@@ -6,11 +7,22 @@ import datetime
 import os
 import shutil
 import urllib
+import time
 
 URL_OFFICIAL_HERO_LIST = 'https://www.dota2.com/datafeed/herolist?language=koreana'
 URL_OPENDOTA_HERO_LIST = 'https://api.opendota.com/api/heroStats'
 URL_OPENDOTA_MATCH_UP = 'https://api.opendota.com/api/heroes/{hero_id}/matchups'
+URL_OPENDOTA_HEROES_LANEROLES = 'https://api.opendota.com/api/scenarios/laneRoles'
 URL_IMAGE_URL = 'https://api.opendota.com/apps/dota2/images/dota_react/heroes'
+
+
+def request(url):
+    HEADERS = {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+    }
+
+    response = requests.get(url, headers=HEADERS)
+    return response
 
 
 def get_db_file_name():
@@ -61,7 +73,7 @@ def create_hero_table():
 
 
 def to_png(path: str, url: str, name: str):
-    response = requests.get(f"https://api.opendota.com{url}")
+    response = request(f"https://api.opendota.com{url}")
 
     with open(f"{path}/{name}.png", 'wb') as f:
         f.write(response.content)
@@ -69,7 +81,8 @@ def to_png(path: str, url: str, name: str):
 
 def delete_hero_data():
     db = open_db()
-    data = db.execute("select * from sqlite_master where name='Hero'").fetchone()
+    data = db.execute(
+        "select * from sqlite_master where name='Hero'").fetchone()
     if data is None:
         return
 
@@ -114,6 +127,77 @@ def insert_hero_data(id_, name, localized_name_eng, localized_name_kor, primary_
     conn.close()
 
 
+# URL_OPENDOTA_HEROES_LANEROLES 에 접속해서 laneRoles 정보를 가져온다.
+def get_lane_roles(hero_id):
+    print(f"URL: {URL_OPENDOTA_HEROES_LANEROLES}?hero_id={hero_id}")
+
+    response = request(
+        f"{URL_OPENDOTA_HEROES_LANEROLES}?hero_id={hero_id}")
+
+    lane_roles = response.json()
+
+    df = None
+    try:
+        df = pd.DataFrame(lane_roles)
+    except Exception as e:
+        print(f"Error 발생 : {lane_roles}")
+        return None
+
+        # games 헤더를 int 형으로 변경
+    df['games'] = df['games'].astype(int)
+
+    # games 헤더를 기준으로 정렬
+    df.sort_values(by=['games'], ascending=False, inplace=True)
+
+    # id_x 컬럼을 hero_id로 변경
+    df.rename(columns={'id_x': 'hero_id'}, inplace=True)
+
+    # games 컬럼에 상위 3개 추출
+    df = df.head(3)
+
+    # games 컬럼에 중복 제거
+    df = df.drop_duplicates(subset=['games'])
+
+    # df 에서 순서대로 변수에 저장, 없으면 None
+    lane_role_1 = df.iloc[0]['lane_role'] if len(df) > 0 else None
+    lane_role_2 = df.iloc[1]['lane_role'] if len(df) > 1 else None
+    lane_role_3 = df.iloc[2]['lane_role'] if len(df) > 2 else None
+
+    # lane_role_1, lane_role_2, lane_role_3 을 데이터프레임으로 변환
+    df_lane_role = pd.DataFrame(
+        {'lane_role_1': [lane_role_1], 'lane_role_2': [lane_role_2], 'lane_role_3': [lane_role_3]})
+    df_lane_role = df_lane_role.fillna('')
+
+    # hero_id 컬럼 추가
+    df_lane_role['hero_id'] = hero_id
+
+    return df_lane_role
+
+
+def calculate_lane_roles(hero_df):
+    # lane_role_1, lane_role_2, lane_role_3 컬럼 추가
+    hero_df['lane_role_1'] = ''
+    hero_df['lane_role_2'] = ''
+    hero_df['lane_role_3'] = ''
+
+    for i, hero_id in hero_df['id_x'].iteritems():
+        lane_roles = get_lane_roles(hero_id)
+        # lane_roles 가 None 이면 60초 후 다시 시도
+        if lane_roles is None:
+            print(f"lane_roles is None, hero_id: {hero_id} 60초 후 다시 시도")
+            time.sleep(60)
+            lane_roles = get_lane_roles(hero_id)
+
+        # lane_roles 데이터프레임을 hero_df에 hero_id 기준으로 lane_role1, lane_role2, lane_role3 컬럼에 추가
+        hero_df.loc[hero_df['id_x'] == hero_id, 'lane_role_1'] = lane_roles['lane_role_1'].values[0]
+        hero_df.loc[hero_df['id_x'] == hero_id, 'lane_role_2'] = lane_roles['lane_role_2'].values[0]
+        hero_df.loc[hero_df['id_x'] == hero_id, 'lane_role_3'] = lane_roles['lane_role_3'].values[0]
+
+        time.sleep(0.5)
+
+    return hero_df
+
+
 def get_local_heroes():
     db = open_db()
     d = db.execute('select * from Hero').fetchall()
@@ -125,12 +209,12 @@ def get_local_heroes():
 
 
 def get_official_heroes_list():
-    response = requests.get(URL_OFFICIAL_HERO_LIST)
+    response = request(URL_OFFICIAL_HERO_LIST)
     return response.json()['result']['data']['heroes']
 
 
 def get_opendota_heroes_list():
-    response = requests.get(URL_OPENDOTA_HERO_LIST)
+    response = request(URL_OPENDOTA_HERO_LIST)
     return response.json()
 
 
@@ -172,7 +256,8 @@ def update_hero_tier_column(hero_df, matches_df):
         win_rate_df = (hero_df[f"{i}_win"] / hero_df[f"{i}_pick"]) * 100
         win_rate_df.sort_values(ascending=False, inplace=True)
 
-        pick_rate_df = (hero_df[f"{i}_pick"] / matches_df[f"{i}_pick"][0]) * 1000
+        pick_rate_df = (hero_df[f"{i}_pick"] /
+                        matches_df[f"{i}_pick"][0]) * 1000
         pick_rate_df.sort_values(ascending=False, inplace=True)
 
         # 그 외 5티어
@@ -223,7 +308,6 @@ def get_tier_index(win_rate_df, pick_rate_df, win_rate, pick_rate):
 
 
 def calculate_match_ups(hero_df):
-
     # hero_df.
 
     # for i, hero in hero_df.iteritems():
@@ -259,6 +343,8 @@ def create_db():
     hero_stats = update_hero_tier_column(hero_stats, matches)
 
     hero_stats = calculate_match_ups(hero_stats)
+
+    hero_stats = calculate_lane_roles(hero_stats)
 
     matches.to_sql('Matches', conn, if_exists='replace')
     hero_stats.to_sql('Hero', conn, if_exists='replace')
