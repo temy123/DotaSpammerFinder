27d055b5ab64b0377d663b3ce386e1b895505fe3
hyunwoo Lee <temy123@naver.com>
2023-09-28 04:26:17 +0900

    아카이브 저장용 Repo 에 업로드 하는 부분 추가

--- DbMaker/update.py
+++ DbMaker/update.py
@@ -1,6 +1,8 @@
+import json
 import os
 import shutil
 import datetime
+import base64
 import main
 import pandas as pd
 import requests
@@ -13,7 +15,8 @@ def get_str_today():
 # 아카이브 전용 레포에 등록
 def upload_to_archive_repo(db_path):
     # GitHub personal access token 설정
-    GITHUB_TOKEN = 'SHA256:tJtenSS7kj82bDFYA/5ApjdldoxQS7WzS2Fq6iS2VCk'
+    # 2024.09.28 삭제, 갱신 필요
+    GITHUB_TOKEN = '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'
 
     # 업로드할 리포지토리 정보 설정
     repository_owner = 'temy123'
@@ -22,25 +25,28 @@ def upload_to_archive_repo(db_path):
 
     # 파일을 업로드할 리포지토리 API URL 생성
     url_template = f'https://api.github.com/repos/{repository_owner}/{repository_name}/contents/'
-
     # 모든 파일을 업로드
     with open(db_path, 'rb') as file:
         file_content = file.read()
 
+    encoded_content = base64.b64encode(file_content).decode()
+
     # 업로드할 파일의 경로 및 커밋 메시지 설정
-    file_path = f'{db_path}'
-    commit_message = f'[{get_str_today()}] Upload {db_path}'
+    file_path = f'od.db'
+    commit_message = f'[{get_str_today()}] Upload'
 
     # GitHub API를 사용하여 파일 업로드
     headers = {
         'Authorization': f'token {GITHUB_TOKEN}',
-        'Content-Type': 'application/json',
+        "Accept": "application/vnd.github.v3+json"
+        # 'Content-Type': 'application/json',
     }
     data = {
         'message': commit_message,
-        'content': file_content,
+        'content': encoded_content,
         'branch': branch_name,
     }
+    # data = json.dumps(data)
     response = requests.put(url_template + file_path, headers=headers, json=data)
 
     if response.status_code == 201:
